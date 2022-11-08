c10afeef9e67fd8019431a49d5ac90287be65529
slavon <vaceslavmartynov325@gmail.com>
2022-11-08 22:04:16 +0300

    fix: small fix with rubocop

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-    factory :answer do
-      body { "MyText" }
-      question { Question.create!(title: "Question", body: 'Question body') }
-    end
+  factory :answer do
+    body { "MyText" }
+    question { Question.create!(title: "Question", body: "Question body") }
+  end
 end
