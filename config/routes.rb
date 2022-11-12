f34c31e4a2cbe2af5906dba963d8b070ca9f3823
slavon <vaceslavmartynov325@gmail.com>
2022-11-12 23:35:44 +0300

    feat: answers controller with specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions, shallow: true do
+    resources :answers, except: :index
+  end
 
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :answer do
     body { "MyText" }
     question { Question.create!(title: "Question", body: "Question body") }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
