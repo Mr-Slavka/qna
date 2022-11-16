deb97c5194717b8ae3206b329241777c6b438f0c
slavon <vaceslavmartynov325@gmail.com>
2022-11-16 18:18:23 +0300

    feat: test for question and answers to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_132546) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_151438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_132546) do
     t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -44,5 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_132546) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
 end
