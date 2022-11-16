c5266988e34bcb3f5382df4f8a7c837d9ad78811
slavon <vaceslavmartynov325@gmail.com>
2022-11-16 18:12:49 +0300

    feat: add list of questions, delete question test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,7 +17,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = current_user.questions.new(question_params)
 
     if @question.save
       redirect_to question_path(@question) , notice: 'Your question successfully created.'
@@ -35,8 +35,12 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question.destroy
-    redirect_to questions_path
+    if current_user.author?(@question)
+      @question.destroy
+      redirect_to questions_path, notice: 'Question successfully deleted.'
+    else
+      redirect_to questions_path, notice: 'You have no rights to delete this question.'
+    end
   end
 
   private
