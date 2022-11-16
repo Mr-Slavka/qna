4237e7642a94a19f74a68816bf5c2114ed164c9c
slavon <vaceslavmartynov325@gmail.com>
2022-11-16 19:00:49 +0300

    feat: create answer test

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  let(:question) { create(:question) }
+  let(:user) { create(:user) }
+  let!(:answer) { create(:answer, question: question, user: user) }
+  let!(:question) { create(:question, user: user) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 
   describe 'POST #create' do
+    before { login(user) }
     context 'with valid attributes' do
-      it 'saves a new answer in the database' do
+      it 'Authenticated user saves a new answer in the database' do
         expect { post :create, params: {question_id: question, answer: attributes_for(:answer)} }.to change(question.answers, :count).by(1)
       end
 
@@ -16,7 +27,7 @@ RSpec.describe AnswersController, type: :controller do
     end
 
     context 'with invalid attributes' do
-      it 'does not save the answer' do
+      it 'Authenticated user does not save the answer' do
         expect { post :create, params: {question_id: question, answer: attributes_for(:answer, :invalid)} }.to_not change(question.answers, :count)
       end
 
