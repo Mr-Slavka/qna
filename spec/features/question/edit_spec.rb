33caff6cd4bd2a82158e801fbdd9a7fdf32ed88c
slavon <vaceslavmartynov325@gmail.com>
2022-11-21 20:13:00 +0300

    feat: add tests to attach files for editing question

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'User can edit his question', %{
+feature "User can edit his question", %{
   In order to correct mistakes
   As an author of question
   I'd like ot be able to edit my question
@@ -9,50 +9,64 @@ feature 'User can edit his question', %{
   given!(:question) { create(:question, user: user) }
   given!(:other_user) { create(:user) }
 
-  scenario 'Unauthenticated user can not edit question' do
+  scenario "Unauthenticated user can not edit question" do
     visit questions_path
 
-    expect(page).to_not have_link 'Edit question'
+    expect(page).to_not have_link "Edit question"
   end
 
-  describe 'Authenticated user', js: true do
-    scenario 'edits own question' do
-      sign_in user
-      visit question_path(question)
+  describe "Authenticated user" do
+    describe "Author" do
+      background do
+        sign_in user
+        visit question_path(question)
+      end
 
-      click_on 'Edit question'
+      scenario "edits own question", js: true do
+        click_on "Edit question"
 
-      within '.question' do
-        fill_in 'Your question', with: 'edited question title'
-        fill_in 'Body question', with: 'edited question body'
-        click_on 'Save question'
+        within ".question" do
+          fill_in "Your question", with: "edited question title"
+          fill_in "Body question", with: "edited question body"
+          click_on "Save question"
 
-        expect(page).to have_content 'edited question title'
-        expect(page).to_not have_selector 'textarea'
+          expect(page).to have_content "edited question title"
+          expect(page).to_not have_selector "textarea"
+        end
+      end
+
+      scenario "edits own question with errors", js: true do
+        click_on "Edit question"
+
+        within ".question" do
+          fill_in "Your question", with: ""
+
+          click_on "Save question"
+          expect(page).to have_content "Title can't be blank"
+        end
+      end
+
+      scenario "edits his question with files", js: true do
+        within ".question" do
+          click_on "Edit question"
+          fill_in "Your question", with: "edited question title"
+          fill_in "Body question", with: "edited question body"
+          attach_file "File", ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on "Save question"
+
+          expect(page).to have_link "rails_helper.rb"
+          expect(page).to have_link "spec_helper.rb"
+        end
       end
     end
 
-    scenario 'edits own question with errors' do
-      sign_in user
-      visit question_path(question)
-
-      click_on 'Edit question'
-
-      within '.question' do
-        fill_in 'Your question', with: ''
-
-        click_on 'Save question'
-        expect(page).to have_content "Title can't be blank"
-      end
-    end
-
-    scenario 'tries to edit question of other user' do
+    scenario "tries to edit question of other user" do
       sign_in other_user
       visit question_path(question)
 
       expect(page).to have_content question.title
       expect(page).to have_content question.body
-      expect(page).to_not have_link 'Edit question'
+      expect(page).to_not have_link "Edit question"
     end
   end
 end
