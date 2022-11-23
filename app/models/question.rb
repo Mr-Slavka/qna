4384708b6505bafa2b10bbd37cdb3ac9b7b7ab53
slavon <vaceslavmartynov325@gmail.com>
2022-11-23 21:49:56 +0300

    feat: add links into the question file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,10 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  has_many :links, dependent: :destroy
 
   has_many_attached :files
+
+  accepts_nested_attributes_for :links
 
   belongs_to :user
 
