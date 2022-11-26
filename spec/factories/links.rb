90075248a5fdbea018ec73b6a29586fc80c60969
slavon <vaceslavmartynov325@gmail.com>
2022-11-26 17:11:36 +0300

    feat: add tests for nested and polymorphic forms

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,18 @@
 FactoryBot.define do
   factory :link do
     name { "MyString" }
-    url { "MyString" }
+    url { "https://google.ru" }
+  end
+
+  trait :linkable do
+    association :linkable, factory: :answer
+  end
+
+  trait :valid_gist do
+    url { 'https://gist.github.com/Mr-Slavka/77ab58aa336e5229098af7df33230dbb' }
+  end
+
+  trait :invalid_gist do
+    url { 'https://gist.github.com/Mr-Slavka/' }
   end
 end
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -1,25 +1,120 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'User can add links to question', %q{
+feature "User can add links to question", %q{
   In order to provide additional info to my question
   As an question's author
   I'd like to be able to add links
 } do
   given(:user) { create(:user) }
-  given(:gist_url) { 'https://gist.github.com/Mr-Slavka/77ab58aa336e5229098af7df33230dbb' }
+  given(:url) {"https://google.com"}
+  given(:url_1) {"https://yahoo.com"}
+  given(:gist_url) { "https://gist.github.com/Mr-Slavka/77ab58aa336e5229098af7df33230dbb" }
+  given(:gist_url_1) { "https://gist.github.com/Mr-Slavka/3cb68368b89a6a81add6e0db32c030d6" }
+  given(:invalid_url) { "invalid_url" }
+  given!(:question) { create(:question, user: user) }
 
-  scenario 'User adds link when asks question' do
+  describe "Authenticated user" do
+    background do
+      sign_in(user)
+      visit new_question_path
+    end
+
+    scenario "adds link asking question" do
+      fill_in "Title", with: "Test question"
+      fill_in "Body", with: "text text text"
+
+      fill_in "Link name", with: "My gist"
+      fill_in "Url", with: url
+
+      click_on "Ask"
+
+      expect(page).to have_link "My gist", href: url
+    end
+
+    scenario "adds many links asking question", js: true do
+      fill_in "Title", with: "Test question"
+      fill_in "Body", with: "text text text"
+
+      fill_in "Link name", with: "My gist"
+      fill_in "Url", with: url
+
+      click_on "add link"
+
+      within ".nested-fields" do
+        fill_in "Link name", with: "My gist 1"
+        fill_in "Url", with: url_1
+      end
+
+      click_on "Ask"
+
+      expect(page).to have_link "My gist", href: url
+      expect(page).to have_link "My gist 1", href: url_1
+    end
+
+    scenario "tries to add invalid url address", js: true do
+      fill_in "Title", with: "Test question"
+      fill_in "Body", with: "text text text"
+
+      fill_in "Link name", with: "My gist", match: :prefer_exact
+      fill_in "Url", with: invalid_url, match: :prefer_exact
+
+      click_on "Ask"
+      expect(page).to have_content "Links url is invalid"
+    end
+
+    scenario "can add new link to gist editing question", js: true do
+
+      fill_in "Title", with: "Test question"
+      fill_in "Body", with: "text text text"
+
+      fill_in "Link name", with: "gist"
+      fill_in "Url", with: gist_url
+
+      click_on "Ask"
+
+
+      expect(page).to have_link "gist", href: gist_url
+    end
+  end
+
+  scenario "can add new links to gist editing question", js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    click_on "Edit question"
+
+    fill_in "Title", with: "edited question title", match: :prefer_exact
+    fill_in "Body", with: "edited question body", match: :prefer_exact
+
+    click_on "add link", match: :prefer_exact
+
+
+    fill_in "Link name", with: "My gist", match: :prefer_exact
+    fill_in "Url", with: gist_url, match: :prefer_exact
+
+
+    click_on "Save"
+
+
+    expect(page).to have_link "My gist", href: gist_url
+  end
+
+  scenario "deletes link editing question", js: true do
     sign_in(user)
     visit new_question_path
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+    fill_in "Title", with: "Test question"
+    fill_in "Body", with: "text text text"
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    click_on "add link"
 
-    click_on 'Ask'
+    fill_in "Link name", with: "My url", match: :prefer_exact
+    fill_in "Url", with: url_1, match: :prefer_exact
 
-    expect(page).to have_link 'My gist', href: gist_url
+    click_on "Ask"
+
+    click_on "Delete link"
+
+    expect(page).to_not have_link "My url", href: url_1
   end
 end
