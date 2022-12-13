eefacd3160f7e04dc5d3cb230b2e1097ce16c924
slavon <vaceslavmartynov325@gmail.com>
2022-12-13 11:51:45 +0300

    feat: add authentication with github and vkontakte

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should have_many(:rewards).dependent(:destroy) }
+  it { should have_many(:authorizations).dependent(:destroy) }
 
   let(:user) { create(:user) }
   let(:question) { create(:question, user: user) }
@@ -33,6 +34,18 @@ RSpec.describe User, type: :model do
       question = create(:question)
 
       expect(user).to_not be_author(question)
+    end
+  end
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'telegram', uid: '123456') }
+    let(:service) { double('Services::FindForOauth') }
+
+    it 'calls Services::FindForOauth' do
+      expect(Services::FindForOauth).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      User.find_for_oauth(auth)
     end
   end
 end
