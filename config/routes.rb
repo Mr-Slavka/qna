71f3c66115325af0f84ba883951bf619adb524ad
slavon <vaceslavmartynov325@gmail.com>
2022-12-19 22:41:48 +0300

    feat: rest api with doorkeeper oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
 
   devise_for :users, controllers: { omniauth_callbacks: 'oauth_callbacks', confirmations: 'confirmations' }
 
@@ -19,6 +20,18 @@ Rails.application.routes.draw do
   resources :questions, concerns: %i[vote comment], shallow: true do
     resources :answers, concerns: %i[vote comment], except: [:index, :show, :edit] do
       post :mark_as_best, on: :member
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: :index do
+        get :me, on: :collection
+      end
+
+      resources :questions, only: %i[index show create update destroy] do
+        resources :answers, shallow: true, only: %i[index show create update destroy]
+      end
     end
   end
 
