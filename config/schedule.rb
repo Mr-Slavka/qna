fca7aa0fc7ed34f4bf112870e831f53943182d68
slavon <vaceslavmartynov325@gmail.com>
2023-01-11 13:25:41 +0300

    feat : Set sphinx index schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,8 +13,12 @@
 #   rake "some:great:rake:task"
 # end
 #
-  every 1.day do
-   runner "DailyDigestJob.perform_now"
-  end
+every 1.day do
+  runner "DailyDigestJob.perform_now"
+end
+
+every 30.minutes do
+  rake 'ts:index'
+end
 
 # Learn more: http://github.com/javan/whenever
