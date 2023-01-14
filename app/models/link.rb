3cc4a741b429ebf4cd68f090dc5a33713f5ae85f
slavon <vaceslavmartynov325@gmail.com>
2023-01-14 21:29:50 +0300

    Revert "feat: Caching"
    This reverts commit 61ad916c4ae94c50812b0b408b92cbeb5cf29bcc.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
-  belongs_to :question, optional: true, touch: true
-  belongs_to :answer, optional: true, touch: true
+  belongs_to :question, optional: true
+  belongs_to :answer, optional: true
 
   validates :name, :url, presence: true
   validates :url, format: URI::regexp(%w[http https])
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Qna
     config.active_job.queue_adapter = :sidekiq
     config.autoload_paths += [config.root.join('app')]
     config.action_cable.disable_request_forgery_protection = false
-    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
 
     config.generators do |g|
       g.test_framework :rspec,
