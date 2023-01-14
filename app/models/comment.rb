8e0dbc950ba67db8664bad6001f73a42378cabd1
slavon <vaceslavmartynov325@gmail.com>
2023-01-14 21:29:50 +0300

    Revert "feat: Caching"
    This reverts commit 61ad916c4ae94c50812b0b408b92cbeb5cf29bcc.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :question, optional: true, touch: true
-  belongs_to :answer, optional: true, touch: true
+  belongs_to :question, optional: true
+  belongs_to :answer, optional: true
   belongs_to :user
 
   validates :body, presence: true
